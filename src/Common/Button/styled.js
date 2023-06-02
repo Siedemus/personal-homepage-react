652ec31709cad6785b6360d070e5325e0aab5db5
Bartosz Sakiewa <barsak1177@gmail.com>
2023-06-02 14:32:06 +0200

    add new button styles

--- src/Common/Button/styled.js
+++ src/Common/Button/styled.js
@@ -1,6 +1,23 @@
 import styled from "styled-components";
 
 export const StyledButton = styled.button`
-border: none;
-background-color: ${({theme}) => theme.colors.scienceBlue};
-`
+  border: none;
+  background-image: ${({ theme }) => theme.gradients.second};
+  background-size: 290%;
+  background-position: left;
+  width: 138px;
+  height: 46px;
+  border-radius: 4px;
+  color: ${({theme}) => theme.colors.white};
+  transition: 1s;
+  display: flex;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin: 24px 0 4px 0;
+
+  &:hover {
+    background-position: right;
+  }
+`;
