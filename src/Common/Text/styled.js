e8eccb0571731808a734ca58525e6ed1d55bb85e
Bartosz Sakiewa <barsak1177@gmail.com>
2023-06-02 14:32:40 +0200

    change font size and add color

--- src/Common/Text/styled.js
+++ src/Common/Text/styled.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const StyledText = styled.p`
   font-style: normal;
   font-weight: 400;
-  font-size: 20px;
+  font-size: 17px;
   line-height: 140%;
   letter-spacing: 0.05em;
+  color: ${({ theme }) => theme.colors.slateGray};
 `;
