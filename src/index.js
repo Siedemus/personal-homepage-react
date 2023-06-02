eed1da932f28c0942a3ac3a0f3c12861645d79f3
Bartosz Sakiewa <barsak1177@gmail.com>
2023-06-02 10:00:48 +0200

    add theme,  global styles and google fonts

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./globalStyles";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme = {
+  colors: {
+    whiteLilac: "#FBFBFE",
+    white: "#FFFFFF",
+    scienceBlue: "#0366D6",
+    mineShaft: "#252525",
+    slateGray: "#6E7E91",
+  },
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <GlobalStyles />
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
